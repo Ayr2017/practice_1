b9a13eb70fe7b6d2426102824dadc6947087d87b
Ayrat <ayrat.2013@yandex.ru>
2022-06-19 08:57:39 +0300

    FIX sql

--- db/select/first.sql
+++ db/select/first.sql
@@ -1,13 +1,14 @@
 SELECT books.id,
        books.name,
-       ct.name,
+       ct.name as cover_type,
        GROUP_CONCAT(genres.name) as genres_names,
        COUNT(genres.name)        as genres_count,
-       books.circulation
+       circulation
 FROM books
          JOIN books_has_genres bhg on books.id = bhg.book_id
          JOIN genres ON genres.id = bhg.genre_id
          JOIN cover_types ct on books.cover_type_id = ct.id
-GROUP BY books.id
+WHERE books.cover_type_id = 1
+GROUP BY books.id, circulation
 HAVING genres_count > 3
-   AND books.circulation > 1000;
+   AND circulation > 1000;
