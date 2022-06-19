0ac1890290a2c85f800db5b5e5272355e867872c
Ayrat <ayrat.2013@yandex.ru>
2022-06-19 08:13:42 +0300

    FIX second sql query

--- db/select/second.sql
+++ db/select/second.sql
@@ -1,5 +1,5 @@
-SELECT DISTINCT CONCAT(LEAST(b1_name, b2_name))                                   as name_1,
-                CONCAT(GREATEST(b1_name, b2_name))                                as name_2,
+SELECT DISTINCT LEAST(b1_name, b2_name)                                   as name_1,
+                GREATEST(b1_name, b2_name)                                as name_2,
                 COUNT(bhg1_genre_id)                                              as genres_count,
                 GROUP_CONCAT(DISTINCT g_name ORDER BY g_name DESC SEPARATOR ', ') as genres_name
 FROM (SELECT bhg1.book_id  as bhg1_book_id,
@@ -11,7 +11,7 @@ FROM (SELECT bhg1.book_id  as bhg1_book_id,
              bhg2.genre_id
       FROM books_has_genres bhg1
                JOIN books_has_genres bhg2 ON bhg1.book_id != bhg2.book_id AND bhg1.genre_id = bhg2.genre_id
-JOIN books b1
+               JOIN books b1
       ON b1.id = bhg1.book_id
           JOIN books b2 ON b2.id = bhg2.book_id
           JOIN genres g ON g.id = bhg2.genre_id) s1
